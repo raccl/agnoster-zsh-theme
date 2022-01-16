471b598ceeaaa69a8c4935132e7f19b78a95e3ff
TaYaKi71751 <shantian.tw@gmail.com>
2022-01-17 03:10:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" \
+# Replace robbyrussell to agnoster
+sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
+# Install agnoster
 curl -LsSf https://raw.githubusercontent.com/raccl/agnoster-zsh-theme/master/agnoster.zsh-theme -o ~/.oh-my-zsh/themes/agnoster.zsh-theme
